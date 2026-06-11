6f7457eb6217296b2d5504ede32599a90dbf7e12
HKaanKaya <kayahayrettinkaan@gmail.com>
2026-06-12 01:00:55 +0300

    Feature: Scrapper can reach the price and title of the product

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ from src.scrapper import PriceScrapper
 
 bot = PriceScrapper()
 
-bot.scrape_product("https://www.whatsmyua.info")
+product = bot.scrape_product("https://www.kitapyurdu.com/kitap/percy-jackson-ve-olimposlular-simsek-hirsizi/657557.html?filter_name=şimşek+hırsızı&s_token=1580419f36b3bd927e7d23c54eb0c1b3479faa60eaa5c85b21ccd545aba82b28&s_time=1781214248")
+
+print(product)
--- src/scrapper.py
+++ src/scrapper.py
@@ -1,11 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
 class PriceScrapper():
 
     def __init__(self):
         self.headers = {"User-agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/26.4 Safari/605.1.15"}
 
+        
 
     def scrape_product(self,url):
         
-        print(f"{url} bilgisi ekrana yazılıyor...")
+        response = requests.get(url, headers=self.headers)
+        response.encoding = "utf-8"
+        soup = BeautifulSoup(response.text,'html.parser')
+        title_element = soup.find("h1")
+        raw_title = title_element.text.strip()
+        price_element = soup.find(class_="price__item")
+        raw_price = float(price_element.text.strip().replace(",","."))
         
-        return None
+        informations = {
+            "title" : raw_title,
+            "price" : raw_price
+        }        
+
+        return informations
+        
+
+
+        
